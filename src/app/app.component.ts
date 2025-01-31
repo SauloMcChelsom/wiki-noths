172278d8335cdb0ae9013ab13584e0a0efdc5298
Saulo McChelsom <maellyramos@gmail.com>
2025-01-31 17:39:22 -0300

    add module material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AngularMaterialModule } from '@core/theme/angular-material.module';
 
 @Component({
-  imports: [RouterModule],
+  imports: [RouterModule, AngularMaterialModule],
   selector: 'app-root',
   template: '<router-outlet></router-outlet>',
   standalone: true,
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(
+    private renderer: Renderer2, 
+  ) {
+    this.renderer.addClass(document.body, 'laranjinha');
+    this.renderer.addClass(document.body, 'dark');//dark/light
+  }
+}
